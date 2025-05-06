5293da1b608893c22d88f46f419c10b8612b5443
ElizabethHuang <lizne6z0@gmail.com>
2025-05-06 22:51:02 +0800

    refactor: Add constants map.

--- msgpack/decode.go
+++ msgpack/decode.go
@@ -8,6 +8,14 @@ import (
 	"slices"
 )
 
+var LastByte = map[string]byte{
+	"positiveFixint": 0x7f,
+	"negativeFixint": 0xff,
+	"fixstr":         0xbf,
+	"fixarray":       0x9f,
+	"fixmap":         0x8f,
+}
+
 func ToJSON(msgpackconv []byte) []byte {
 	if len(msgpackconv) == 0 {
 		return []byte{}
@@ -28,15 +36,15 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 	v := reflect.ValueOf(&obj).Elem()
 	// 已讀取的 byte index
 	idxOfEnd := 0
-	if msgpackconv[0] >= 0xa0 && msgpackconv[0] <= 0xbf {
+	if msgpackconv[0] >= FirstByte["fixstr"] && msgpackconv[0] <= LastByte["fixstr"] {
 		// fixstr
-		l := getLength([]byte{msgpackconv[0] ^ 0xa0})
+		l := getLength([]byte{msgpackconv[0] ^ FirstByte["fixstr"]})
 		if len(msgpackconv) < l+1 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(string(msgpackconv[1 : 1+l])))
 		idxOfEnd = l + 1
-	} else if msgpackconv[0] == 0xd9 {
+	} else if msgpackconv[0] == FirstByte["str8"] {
 		// str8
 		if len(msgpackconv) < 2 {
 			return nil, 0, ErrInvalidMsgPack
@@ -47,7 +55,7 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(string(msgpackconv[2 : 2+l])))
 		idxOfEnd = l + 2
-	} else if msgpackconv[0] == 0xda {
+	} else if msgpackconv[0] == FirstByte["str16"] {
 		// str16
 		if len(msgpackconv) < 3 {
 			return nil, 0, ErrInvalidMsgPack
@@ -58,8 +66,8 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(string(msgpackconv[3 : 3+l])))
 		idxOfEnd = l + 3
-	} else if msgpackconv[0] == 0xdb {
-		// str16
+	} else if msgpackconv[0] == FirstByte["str32"] {
+		// str32
 		if len(msgpackconv) < 5 {
 			return nil, 0, ErrInvalidMsgPack
 		}
@@ -69,99 +77,99 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(string(msgpackconv[5 : 5+l])))
 		idxOfEnd = l + 5
-	} else if msgpackconv[0] >= 0x00 && msgpackconv[0] <= 0x7f {
+	} else if msgpackconv[0] >= FirstByte["positiveFixint"] && msgpackconv[0] <= LastByte["positiveFixint"] {
 		// positive fixint
 		v.Set(reflect.ValueOf(bytesToFloat64([]byte{msgpackconv[0]}, true)))
 		idxOfEnd = 1
-	} else if msgpackconv[0] == 0xcc {
+	} else if msgpackconv[0] == FirstByte["uint8"] {
 		// uint8
 		if len(msgpackconv) < 2 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:2], true)))
 		idxOfEnd = 2
-	} else if msgpackconv[0] == 0xcd {
+	} else if msgpackconv[0] == FirstByte["uint16"] {
 		// uint16
 		if len(msgpackconv) < 3 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:3], true)))
 		idxOfEnd = 3
-	} else if msgpackconv[0] == 0xce {
+	} else if msgpackconv[0] == FirstByte["uint32"] {
 		// uint32
 		if len(msgpackconv) < 5 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:5], true)))
 		idxOfEnd = 5
-	} else if msgpackconv[0] == 0xcf {
+	} else if msgpackconv[0] == FirstByte["uint64"] {
 		// uint64
 		if len(msgpackconv) < 9 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:9], true)))
 		idxOfEnd = 9
-	} else if msgpackconv[0] >= 0xe0 {
+	} else if msgpackconv[0] >= FirstByte["negativeFixint"] {
 		// negative fixint
 		v.Set(reflect.ValueOf(bytesToFloat64([]byte{msgpackconv[0]}, false)))
 		idxOfEnd = 1
-	} else if msgpackconv[0] == 0xd0 {
+	} else if msgpackconv[0] == FirstByte["int8"] {
 		// int8
 		if len(msgpackconv) < 2 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:2], false)))
 		idxOfEnd = 2
-	} else if msgpackconv[0] == 0xd1 {
+	} else if msgpackconv[0] == FirstByte["int16"] {
 		// int16
 		if len(msgpackconv) < 3 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:3], false)))
 		idxOfEnd = 3
-	} else if msgpackconv[0] == 0xd2 {
+	} else if msgpackconv[0] == FirstByte["int32"] {
 		// int32
 		if len(msgpackconv) < 5 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:5], false)))
 		idxOfEnd = 5
-	} else if msgpackconv[0] == 0xd3 {
+	} else if msgpackconv[0] == FirstByte["int64"] {
 		// int64
 		if len(msgpackconv) < 9 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bytesToFloat64(msgpackconv[1:9], false)))
 		idxOfEnd = 9
-	} else if msgpackconv[0] == 0xca {
+	} else if msgpackconv[0] == FirstByte["float32"] {
 		// float32
 		if len(msgpackconv) < 5 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bitsToFloat32(msgpackconv[1:5])))
 		idxOfEnd = 5
-	} else if msgpackconv[0] == 0xcb {
+	} else if msgpackconv[0] == FirstByte["float64"] {
 		// float64
 		if len(msgpackconv) < 9 {
 			return nil, 0, ErrInvalidMsgPack
 		}
 		v.Set(reflect.ValueOf(bitsToFloat64(msgpackconv[1:9])))
 		idxOfEnd = 9
-	} else if msgpackconv[0] == 0xc0 {
+	} else if msgpackconv[0] == FirstByte["nil"] {
 		// nil
 		// do nothing
 		idxOfEnd = 1
-	} else if msgpackconv[0] == 0xc2 {
+	} else if msgpackconv[0] == FirstByte["false"] {
 		// false
 		v.Set(reflect.ValueOf(false))
 		idxOfEnd = 1
-	} else if msgpackconv[0] == 0xc3 {
+	} else if msgpackconv[0] == FirstByte["true"] {
 		// true
 		v.Set(reflect.ValueOf(true))
 		idxOfEnd = 1
-	} else if msgpackconv[0] >= 0x90 && msgpackconv[0] <= 0x9f {
+	} else if msgpackconv[0] >= FirstByte["fixarray"] && msgpackconv[0] <= LastByte["fixarray"] {
 		// fixarray
-		l := getLength([]byte{msgpackconv[0] ^ 0x90})
+		l := getLength([]byte{msgpackconv[0] ^ FirstByte["fixarray"]})
 		s := make([]interface{}, l)
 		var err error
 		for j := range l {
@@ -172,7 +180,7 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(s))
 		idxOfEnd = l + 1
-	} else if msgpackconv[0] == 0xdc {
+	} else if msgpackconv[0] == FirstByte["array16"] {
 		// array16
 		l := getLength(msgpackconv[1:3])
 		s := make([]interface{}, l)
@@ -185,7 +193,7 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(s))
 		idxOfEnd = l + 3
-	} else if msgpackconv[0] == 0xdd {
+	} else if msgpackconv[0] == FirstByte["array32"] {
 		// array32
 		l := getLength(msgpackconv[1:5])
 		s := make([]interface{}, l)
@@ -198,10 +206,10 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(s))
 		idxOfEnd = l + 5
-	} else if msgpackconv[0] >= 0x80 && msgpackconv[0] <= 0x8f {
+	} else if msgpackconv[0] >= FirstByte["fixmap"] && msgpackconv[0] <= LastByte["fixmap"] {
 		// fixmap
 		m := make(map[string]interface{})
-		l := getLength([]byte{msgpackconv[0] ^ 0x80})
+		l := getLength([]byte{msgpackconv[0] ^ FirstByte["fixmap"]})
 		j := 0
 		for len(m) < l {
 			key, tmp, err := decode(msgpackconv[j+1:])
@@ -218,7 +226,7 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(m))
 		idxOfEnd = j
-	} else if msgpackconv[0] == 0xde {
+	} else if msgpackconv[0] == FirstByte["map16"] {
 		// map16
 		m := make(map[string]interface{})
 		l := getLength(msgpackconv[1:3])
@@ -238,7 +246,7 @@ func decode(msgpackconv []byte) (interface{}, int, error) {
 		}
 		v.Set(reflect.ValueOf(m))
 		idxOfEnd = j
-	} else if msgpackconv[0] == 0xdf {
+	} else if msgpackconv[0] == FirstByte["map32"] {
 		// map32
 		m := make(map[string]interface{})
 		l := getLength(msgpackconv[1:5])
@@ -273,9 +281,9 @@ func bytesToFloat64(bytes []byte, positive bool) float64 {
 func bytesToUint64(bytes []byte, positive bool) uint64 {
 	var copyBytes []byte
 	if positive {
-		copyBytes = slices.Repeat([]byte{0x00}, 8)
+		copyBytes = slices.Repeat([]byte{FirstByte["positiveFixint"]}, 8)
 	} else {
-		copyBytes = slices.Repeat([]byte{0xff}, 8)
+		copyBytes = slices.Repeat([]byte{LastByte["negativeFixint"]}, 8)
 	}
 	copy(copyBytes[8-len(bytes):], bytes)
 	return binary.BigEndian.Uint64(copyBytes)
--- msgpack/encode.go
+++ msgpack/encode.go
@@ -6,6 +6,34 @@ import (
 	"math"
 )
 
+var FirstByte = map[string]byte{
+	"positiveFixint": 0x00,
+	"uint8":          0xcc,
+	"uint16":         0xcd,
+	"uint32":         0xce,
+	"uint64":         0xcf,
+	"negativeFixint": 0xe0,
+	"int8":           0xd0,
+	"int16":          0xd1,
+	"int32":          0xd2,
+	"int64":          0xd3,
+	"float32":        0xca,
+	"float64":        0xcb,
+	"fixstr":         0xa0,
+	"str8":           0xd9,
+	"str16":          0xda,
+	"str32":          0xdb,
+	"nil":            0xc0,
+	"false":          0xc2,
+	"true":           0xc3,
+	"fixarray":       0x90,
+	"array16":        0xdc,
+	"array32":        0xdd,
+	"fixmap":         0x80,
+	"map16":          0xde,
+	"map32":          0xdf,
+}
+
 func FromJSON(bytes []byte) []byte {
 	var obj interface{}
 	err := json.Unmarshal(bytes, &obj)
@@ -22,7 +50,7 @@ func encode(obj interface{}) []byte {
 
 	switch v := obj.(type) {
 	case nil:
-		ans = append(ans, 0xc0)
+		ans = append(ans, FirstByte["nil"])
 	case bool:
 		ans = append(ans, getBoolFormat(v))
 	case string:
@@ -51,25 +79,25 @@ func getStrFormat(v string) []byte {
 	case l < 32:
 		// fixstr
 		ans = make([]byte, 1+l)
-		ans[0] = 0xa0 | byte(len(v))
+		ans[0] = FirstByte["fixstr"] | byte(len(v))
 		copy(ans[1:], []byte(v))
 	case float64(l) < math.Pow(2, 8):
 		// str8
 		ans = make([]byte, 2+l)
-		ans[0] = 0xd9
+		ans[0] = FirstByte["str8"]
 		ans[1] = byte(l)
 		copy(ans[2:], []byte(v))
 	case float64(l) < math.Pow(2, 16):
 		// str16
 		ans = make([]byte, 3+l)
-		ans[0] = 0xda
+		ans[0] = FirstByte["str16"]
 		binary.BigEndian.PutUint16(ans[1:3], uint16(l))
 		copy(ans[3:], []byte(v))
 	default:
 		// str32
 		// because maximum byte size of a String object is (2^32)-1
 		ans = make([]byte, 5+l)
-		ans[0] = 0xdb
+		ans[0] = FirstByte["str32"]
 		binary.BigEndian.PutUint32(ans[1:5], uint32(l))
 		copy(ans[5:], []byte(v))
 	}
@@ -78,9 +106,9 @@ func getStrFormat(v string) []byte {
 
 func getBoolFormat(v bool) byte {
 	if v {
-		return 0xc3
+		return FirstByte["true"]
 	}
-	return 0xc2
+	return FirstByte["false"]
 }
 
 func getNumberFormat(v float64) []byte {
@@ -96,29 +124,29 @@ func getNumberFormat(v float64) []byte {
 func getPositiveIntFormat(v int) []byte {
 	switch {
 	case v < 128:
-		return []byte{0x00 | byte(v)}
+		return []byte{FirstByte["positiveFixint"] | byte(v)}
 	case float64(v) < math.Pow(2, 8):
 		// uint8
 		ans := make([]byte, 2)
-		ans[0] = 0xcc
+		ans[0] = FirstByte["uint8"]
 		ans[1] = byte(v)
 		return ans
 	case float64(v) < math.Pow(2, 16):
 		// uint16
 		ans := make([]byte, 3)
-		ans[0] = 0xcd
+		ans[0] = FirstByte["uint16"]
 		binary.BigEndian.PutUint16(ans[1:3], uint16(v))
 		return ans
 	case float64(v) < math.Pow(2, 32):
 		// uint32
 		ans := make([]byte, 5)
-		ans[0] = 0xce
+		ans[0] = FirstByte["uint32"]
 		binary.BigEndian.PutUint32(ans[1:5], uint32(v))
 		return ans
 	default:
 		// uint64
 		ans := make([]byte, 9)
-		ans[0] = 0xcf
+		ans[0] = FirstByte["uint64"]
 		binary.BigEndian.PutUint64(ans[1:9], uint64(v))
 		return ans
 	}
@@ -127,29 +155,29 @@ func getPositiveIntFormat(v int) []byte {
 func getNegativeIntFormat(v int) []byte {
 	switch {
 	case -v <= 32:
-		return []byte{0xe0 | byte(32+v)}
+		return []byte{FirstByte["negativeFixint"] | byte(32+v)}
 	case float64(-v) < math.Pow(2, 7):
 		// int8
 		ans := make([]byte, 2)
-		ans[0] = 0xd0
+		ans[0] = FirstByte["int8"]
 		ans[1] = byte(v)
 		return ans
 	case float64(-v) < math.Pow(2, 15):
 		// int16
 		ans := make([]byte, 3)
-		ans[0] = 0xd1
+		ans[0] = FirstByte["int16"]
 		binary.BigEndian.PutUint16(ans[1:3], uint16(v))
 		return ans
 	case float64(-v) < math.Pow(2, 31):
 		// int32
 		ans := make([]byte, 5)
-		ans[0] = 0xd2
+		ans[0] = FirstByte["int32"]
 		binary.BigEndian.PutUint32(ans[1:5], uint32(v))
 		return ans
 	default:
 		// int64
 		ans := make([]byte, 9)
-		ans[0] = 0xd3
+		ans[0] = FirstByte["int64"]
 		binary.BigEndian.PutUint64(ans[1:9], uint64(v))
 		return ans
 	}
@@ -158,7 +186,7 @@ func getNegativeIntFormat(v int) []byte {
 func getFloatFormat(v float64) []byte {
 	bits := math.Float64bits(v)
 	ans := make([]byte, 9)
-	ans[0] = 0xcb
+	ans[0] = FirstByte["float64"]
 	binary.BigEndian.PutUint64(ans[1:9], bits)
 	return ans
 }
@@ -168,17 +196,17 @@ func getMapFormat(v map[string]interface{}) []byte {
 	switch {
 	case l < 16:
 		// fixmap
-		return []byte{(0x80 | byte(len(v)))}
+		return []byte{(FirstByte["fixmap"] | byte(len(v)))}
 	case l < int(math.Pow(2, 16)):
 		// map16
 		ans := make([]byte, 3)
-		ans[0] = 0xde
+		ans[0] = FirstByte["map16"]
 		binary.BigEndian.PutUint16(ans[1:3], uint16(l))
 		return ans
 	default:
 		// map32
 		ans := make([]byte, 5)
-		ans[0] = 0xdf
+		ans[0] = FirstByte["map32"]
 		binary.BigEndian.PutUint32(ans[1:5], uint32(l))
 		return ans
 	}
@@ -189,17 +217,17 @@ func getArrayFormat(v []interface{}) []byte {
 	switch {
 	case l < 16:
 		// fixarray
-		return []byte{(0x90 | byte(len(v)))}
+		return []byte{(FirstByte["fixarray"] | byte(len(v)))}
 	case l < int(math.Pow(2, 16)):
 		// array16
 		ans := make([]byte, 3)
-		ans[0] = 0xdc
+		ans[0] = FirstByte["array16"]
 		binary.BigEndian.PutUint16(ans[1:3], uint16(l))
 		return ans
 	default:
 		// array32
 		ans := make([]byte, 5)
-		ans[0] = 0xdd
+		ans[0] = FirstByte["array32"]
 		binary.BigEndian.PutUint32(ans[1:5], uint32(l))
 		return ans
 	}
